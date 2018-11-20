75581fab61858e708ceda3274dae26d1242f4b17
LIWEI487 <LIWEI487@pingan.com>
2018-11-20 15:44:53 +0800

    spring boot热部署支持

--- example/src/main/java/com/example/example/controller/ValidParamController.java
+++ example/src/main/java/com/example/example/controller/ValidParamController.java
@@ -3,6 +3,7 @@ package com.example.example.controller;
 import com.example.example.common.antation.ValidParam;
 import com.example.example.controller.validate.TestFormDataValidate;
 import com.example.example.formData.TestFormData;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,6 +26,15 @@ public class ValidParamController {
     @ValidParam(value = TestFormDataValidate.class, method = "validate", failFast =false )
     public void test1( @RequestBody TestFormData data){
         System.out.println(" success");
+    }
+
+    @GetMapping("test2")
+    public void test2(){
+        System.out.println(" success1");
+        System.out.println(" success");
+        System.out.println(" success2");
+        System.out.println(" success4");
+        System.out.println(" success5");
     }
 
 
